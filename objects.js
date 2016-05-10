27db7358f7249e6de63005a510d67180cb8112ed
Kyle Coberly <kyle.coberly@gmail.com>
2016-05-10 15:53:03 -0600

    Updating IDs to prevent index lookup

--- objects.js
+++ objects.js
@@ -1,28 +1,28 @@
 module.exports = {
     inventory: [{
-        id: 1,
+        id: 10,
         name: "Baseball Bat",
         price: 19.99,
         quantityAvailable: 3
     },{
-        id: 2,
+        id: 15,
         name: "Tennis Racket",
         price: 50.00,
         quantityAvailable: 5
     },{
-        id: 3,
+        id: 18,
         name: "Running Shoes",
         price: 79.99,
         quantityAvailable: 7
     }],
     shoppingCart: [{
-        itemId: 1,
+        itemId: 10,
         quantity: 0
     },{
-        itemId: 2,
+        itemId: 15,
         quantity: 0
     },{
-        itemId: 3,
+        itemId: 18,
         quantity: 0
     }]
 };
--- test/test.js
+++ test/test.js
@@ -14,38 +14,38 @@ describe("Functions should work", function() {
     it("should have a working addItem function", function(){
         initializeData();
 
-        galvanizeSports.addItem(1, 1);
+        galvanizeSports.addItem(10, 1);
         assert.equal(galvanizeSports.shoppingCart[0].quantity, 3, "Item 1 should be added to the shopping cart");
         assert.equal(galvanizeSports.inventory[0].quantityAvailable, 2, "Item 1 should be removed from the available inventory");
 
-        galvanizeSports.addItem(2, 1);
+        galvanizeSports.addItem(15, 1);
         assert.equal(galvanizeSports.shoppingCart[1].quantity, 3, "Items 2 should be added to the shopping cart");
         assert.equal(galvanizeSports.inventory[1].quantityAvailable, 4, "Item 2 should be removed from the available inventory");
 
-        galvanizeSports.addItem(3, 1);
+        galvanizeSports.addItem(18, 1);
         assert.equal(galvanizeSports.shoppingCart[2].quantity, 3, "Items 3 should be added to the shopping cart");
         assert.equal(galvanizeSports.inventory[2].quantityAvailable, 6, "Item 3 should be removed from the available inventory");
 
-        galvanizeSports.addItem(1, 50);
+        galvanizeSports.addItem(10, 50);
         assert.equal(galvanizeSports.shoppingCart[0].quantity, 5, "All available item 1's should be added to the shopping cart");
         assert.equal(galvanizeSports.inventory[0].quantityAvailable, 0, "All available item 1's should be removed from the available inventory");
     });
     it("should have a working removeItem function", function(){
         initializeData();
 
-        galvanizeSports.removeItem(1, 1);
+        galvanizeSports.removeItem(10, 1);
         assert.equal(galvanizeSports.shoppingCart[0].quantity, 1, "Item 1 should be added to the shopping cart");
         assert.equal(galvanizeSports.inventory[0].quantityAvailable, 4, "Item 1 should be removed from the available inventory");
 
-        galvanizeSports.removeItem(2, 1);
+        galvanizeSports.removeItem(15, 1);
         assert.equal(galvanizeSports.shoppingCart[1].quantity, 1, "Items 2 should be added to the shopping cart");
         assert.equal(galvanizeSports.inventory[1].quantityAvailable, 6, "Item 2 should be removed from the available inventory");
 
-        galvanizeSports.removeItem(3, 1);
+        galvanizeSports.removeItem(18, 1);
         assert.equal(galvanizeSports.shoppingCart[2].quantity, 1, "Items 3 should be added to the shopping cart");
         assert.equal(galvanizeSports.inventory[2].quantityAvailable, 8, "Item 3 should be removed from the available inventory");
 
-        galvanizeSports.removeItem(1, 50);
+        galvanizeSports.removeItem(10, 50);
         assert.equal(galvanizeSports.shoppingCart[0].quantity, 0, "All available item 1's should be added to the shopping cart");
         assert.equal(galvanizeSports.inventory[0].quantityAvailable, 5, "All available item 1's should be removed from the available inventory");
     });
@@ -70,10 +70,10 @@ describe("User workflow should work", function() {
     it("should allow the user to add and remove some items, then display the correct total", function(){
         initializeData();
 
-        galvanizeSports.addItem(1, 2);
-        galvanizeSports.addItem(2, 2);
-        galvanizeSports.addItem(3, 3);
-        galvanizeSports.removeItem(2, 2);
+        galvanizeSports.addItem(10, 2);
+        galvanizeSports.addItem(15, 2);
+        galvanizeSports.addItem(18, 3);
+        galvanizeSports.removeItem(15, 2);
 
         assert.equal(galvanizeSports.shoppingCart[0].quantity, 4, "Correct number of item 1s in shopping cart");
         assert.equal(galvanizeSports.shoppingCart[1].quantity, 2, "Correct number of item 2s in shopping cart");
